454fd634ad1d650d7a57586e2a339193e11e00ff
Mohammed Mostafa <tayar.shaeby@gmail.com>
2026-05-01 21:42:53 +0300

    chore: remove debug scripts

--- _debug_replace.js
+++
@@ -1,5 +0,0 @@
-const p = { name: 'id' };
-const repl = `\${${p.name}}`;
-console.log('repl value:', JSON.stringify(repl));
-const result = '/:id'.replace(new RegExp(`:${p.name}`, 'g'), repl);
-console.log('result:', JSON.stringify(result));
